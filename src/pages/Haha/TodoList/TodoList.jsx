2b2b63408d73d5a0c15cd708a1fcca2e141e40b6
haha861924 <qq861924@gmail.com>
2022-02-28 17:55:30 +0800

    feat: 🎸 add layout

--- src/pages/Haha/TodoList/TodoList.jsx
+++ src/pages/Haha/TodoList/TodoList.jsx
@@ -1,5 +1,15 @@
+import { useState } from 'react';
+import AddItems from './AddItems';
+import ItemList from './ItemList';
 function TodoList() {
   return <div>Haha's TodoList</div>;
+  return (
+    <div className="flex h-screen flex-col items-center bg-blue-300 font-Amatic">
+      <div className="my-10 text-3xl font-black text-yellow-300">Todolist</div>
+      <AddItems addItem={addItem} className="bg-red-300" />
+      <ItemList list={list} changeItem={changeItem} delItem={delItem} />
+    </div>
+  );
 }
 
 export default TodoList;
