43c4b00bbee639d45554d020d94970f9bf20b211
haha861924 <qq861924@gmail.com>
2022-02-28 19:48:28 +0800

    refactor: 💡 Input component

--- src/pages/Haha/TodoList/AddItems.jsx
+++ src/pages/Haha/TodoList/AddItems.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import Icon from './Icon';
+import Input from './Input';
 import Button from './Button';
+
 const uuid = () =>
   ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (a) =>
     (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
@@ -24,14 +25,12 @@ function AddItems({ addItem }) {
 
   return (
     <div className="flex items-center justify-center space-x-5">
-      <input
-        type="text"
-        id="addTask"
-        value={text.val}
+      <Input
+        item={text}
         onChange={onChange}
         onKeyDown={onKeyDown}
-        placeholder="input value"
-        className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+        Class="w-full"
+        label="input value"
       />
       <Button onClick={onClick} logo="add" label="add" Class="bg-[#78abb19d]" />
     </div>
