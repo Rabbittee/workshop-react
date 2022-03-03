06fd80ce4180d0c6f5ace3cee8a488d2a07c11af
HuskyHsu <schi.hsu@gmail.com>
2022-03-03 17:15:41 +0800

    refactor: 💡 Item edit toggle

--- src/pages/Husky/TodoList/Item.jsx
+++ src/pages/Husky/TodoList/Item.jsx
@@ -2,22 +2,28 @@ import { DeleteIcon, EditIcon } from './components/icon';
 import { Input } from './components/utils';
 
 export function Item({ task, onClick, clickDelete, clickEdit, editTask }) {
+  function editToggle(edit) {
+    if (edit) {
+      return <Input value={task.editTitle} onInput={(e) => editTask(e.target.value)} />;
+    }
+    return (
+      <label className="flex flex-1 items-center space-x-3 pl-1">
+        <input
+          type="checkbox"
+          className="bg-check h-6 w-6 rounded-md border border-gray-300 bg-white checked:border-transparent focus:outline-none"
+          onChange={onClick}
+        />
+        <span className={'font-normal text-gray-700' + (task.status ? ' line-through' : '')}>
+          {task.title}
+        </span>
+      </label>
+    );
+  }
+
   return (
     <li className="mb-2 flex flex-row border-gray-400">
       <div className="flex flex-1 transform select-none items-center gap-4 rounded-md bg-white p-4 shadow transition duration-500 ease-in-out hover:-translate-y-1 hover:shadow-lg">
-        {!task.edit && (
-          <label className="flex flex-1 items-center space-x-3 pl-1">
-            <input
-              type="checkbox"
-              className="bg-check h-6 w-6 rounded-md border border-gray-300 bg-white checked:border-transparent focus:outline-none"
-              onChange={onClick}
-            />
-            <span className={'font-normal text-gray-700' + (task.status ? ' line-through' : '')}>
-              {task.title}
-            </span>
-          </label>
-        )}
-        {task.edit && <Input value={task.editTitle} onInput={(e) => editTask(e.target.value)} />}
+        {editToggle(task.edit)}
         <button className="flex w-12 justify-end gap-2 text-right">
           {!task.status && <EditIcon onClick={clickEdit} />}
           <DeleteIcon onClick={clickDelete} />
