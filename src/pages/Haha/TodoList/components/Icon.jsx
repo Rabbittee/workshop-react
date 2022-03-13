94ea316a7d71667acf8fe80ae56c860c7b5815cf
haha861924 <qq861924@gmail.com>
2022-03-13 19:40:50 +0800

    refactor: 💡 remove no use className

--- src/pages/Haha/TodoList/components/Icon.jsx
+++ src/pages/Haha/TodoList/components/Icon.jsx
@@ -1,7 +1,7 @@
 import Icon from '../utils/Icon.js';
 
 function Img({ logo, size }) {
-  return <img src={Icon[logo]} className="App-logo" alt="logo" width={size} height={size} />;
+  return <img src={Icon[logo]} alt="logo" width={size} height={size} />;
 }
 
 export default Img;
