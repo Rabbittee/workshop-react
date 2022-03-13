d4d2b18da519b0b2ffaa9688ec29d5bc456cd999
haha861924 <qq861924@gmail.com>
2022-03-13 19:36:50 +0800

    refactor: 💡 class name with clsx use object

--- src/pages/Haha/TodoList/components/Button.jsx
+++ src/pages/Haha/TodoList/components/Button.jsx
@@ -5,7 +5,7 @@ function Button({ className, fn, message, logo, size }) {
   return (
     <button className={clsx(className, 'col-span-1')} onClick={fn}>
       {logo && <Icon logo={logo} size={size} />}
-      <label className={clsx('mx-2 text-white', logo || message === 'Delete' || 'text-gray-600')}>
+      <label className={clsx('mx-2', { 'text-white': logo || message === 'Delete' })}>
         {message}
       </label>
     </button>
--- src/pages/Haha/TodoList/components/Dialog.jsx
+++ src/pages/Haha/TodoList/components/Dialog.jsx
@@ -8,25 +8,23 @@ function Dialog({ cancel, confirm, message }) {
     >
       <div className="absolute inset-0 z-0 bg-black opacity-80"></div>
       <div className="relative  mx-auto my-auto w-full max-w-lg rounded-xl bg-white p-5  shadow-lg ">
-        <>
-          <div className="flex-auto justify-center p-5 text-center">
-            <h2 className="py-4 text-xl font-bold ">{message}</h2>
-          </div>
-          <div className="space-x-4 text-center md:block">
+        <div className="flex-auto justify-center p-5 text-center">
+          <h2 className="py-4 text-xl font-bold ">{message}</h2>
+        </div>
+        <div className="space-x-4 text-center md:block">
+          <Button
+            onClick={cancel}
+            message="Cancel"
+            className="mb-2 rounded-full border bg-white px-5 py-2 text-sm font-medium tracking-wider text-gray-600 shadow-sm hover:bg-gray-100 hover:shadow-lg md:mb-0"
+          />
+          {confirm && (
             <Button
-              fn={cancel}
-              message="Cancel"
-              className="mb-2 rounded-full border bg-white px-5 py-2 text-sm font-medium tracking-wider shadow-sm hover:bg-gray-100 hover:shadow-lg md:mb-0"
+              onClick={confirm}
+              message="Delete"
+              className="mb-2 rounded-full border border-red-500 bg-red-500 px-5 py-2 text-sm font-medium tracking-wider  shadow-sm hover:bg-red-600 hover:shadow-lg md:mb-0"
             />
-            {confirm && (
-              <Button
-                fn={confirm}
-                message="Delete"
-                className="mb-2 rounded-full border border-red-500 bg-red-500 px-5 py-2 text-sm font-medium tracking-wider shadow-sm hover:bg-red-600 hover:shadow-lg md:mb-0"
-              />
-            )}
-          </div>
-        </>
+          )}
+        </div>
       </div>
     </div>
   );
