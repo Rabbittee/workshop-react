79e54706695f7fefb6bc6a0840cec94376a81581
haha861924 <qq861924@gmail.com>
2022-03-25 11:45:13 +0800

    feat: 🎸 icon component

---
+++ src/pages/Haha/Refactor/assets/index.js
@@ -0,0 +1,11 @@
+import { ReactComponent as Add } from './add.svg';
+import { ReactComponent as Check } from './check-green.svg';
+import { ReactComponent as Edit } from './edit.svg';
+import { ReactComponent as Trash } from './trash.svg';
+
+export const Icon = {
+  Add,
+  Check,
+  Edit,
+  Trash,
+};
