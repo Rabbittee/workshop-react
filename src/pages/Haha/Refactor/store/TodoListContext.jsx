01870e8a2938ced476a526207163e529feecfea4
haha861924 <qq861924@gmail.com>
2022-03-28 02:38:26 +0800

    refactor: 💡 add

--- src/pages/Haha/Refactor/store/TodoListContext.jsx
+++ src/pages/Haha/Refactor/store/TodoListContext.jsx
@@ -13,27 +13,26 @@ export function TodoListProvider({ children }) {
   };
 
   const submit = ({ key, target }) => {
-    // 新增子項目
+    // 送出新增
     if (key === 'Enter' && target.value) {
-      const data = target.value.trim();
-      return add(data);
+      return add(target.value);
     }
     return setInput(target.value);
   };
 
   const submitBtn = () => {
-    // 新增子項目的按鈕
-    const data = input.trim();
-    if (!!data) return add(data);
+    // 送出新增的按鈕
+    if (!!input) return add(input);
   };
 
   const add = (data, id = Date.now()) => {
-    setList([...list, { value: data, ...init, id }]);
+    // 新增項目
+    setList([...list, { value: data.trim(), ...init, id }]);
     setInput('');
   };
 
   const edit = ({ target, key }, { id }) => {
-    // 編輯子項目
+    // 編輯項目
     const newList = list.map((task) => {
       task.id === id && (task.value = target.value);
       return task;
@@ -44,7 +43,7 @@ export function TodoListProvider({ children }) {
   };
 
   const editBtn = (item) => {
-    // 編輯子項目的按鈕
+    // 編輯項目的按鈕
     const newList = list.map((task) => {
       task.id === item.id && (task.value = item.value);
       return task;
@@ -54,14 +53,14 @@ export function TodoListProvider({ children }) {
   };
 
   const del = () => {
-    // 刪除子項目
+    // 刪除項目
     const newList = list.filter((el) => el.id !== dialog.deleteID);
     setList(newList);
     setDialog({ show: false, deleteID: {} });
   };
 
   const delBtn = (id) => {
-    // 刪除子項目的按鈕
+    // 刪除項目的按鈕
     setDialog({ show: true, deleteID: id });
   };
 
