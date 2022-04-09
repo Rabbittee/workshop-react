44d004bc7724fa95fa29da939f4f70635c4dccc4
kevinhws <kevinshu1995@gmail.com>
2022-04-09 23:11:24 +0800

    feat: 🎸 Toast

--- src/pages/Kevin/TodoList/TodoList.jsx
+++ src/pages/Kevin/TodoList/TodoList.jsx
@@ -2,22 +2,25 @@ import { TodoProvider } from './components/TodoContext.jsx';
 import TodoAddModal from './components/TodoAddModal.jsx';
 import TodoItems from './components/TodoItems.jsx';
 import TodoSelect from './components/TodoSelect.jsx';
+import { ToastProvider } from './components/Toast.jsx';
 
 function TodoList() {
   return (
     <div className="flex min-h-screen flex-col items-center gap-4 bg-slate-700 px-2 pt-8">
-      <TodoProvider>
-        <h1 className="text-3xl font-bold text-white">Todo List!</h1>
-        <div className="flex w-full max-w-2xl flex-col gap-4">
-          {/* search */}
+      <ToastProvider>
+        <TodoProvider>
+          <h1 className="text-3xl font-bold text-white">Todo List!</h1>
+          <div className="flex w-full max-w-2xl flex-col gap-4">
+            {/* search */}
 
-          <TodoSelect />
+            <TodoSelect />
 
-          <TodoItems />
-        </div>
+            <TodoItems />
+          </div>
 
-        <TodoAddModal />
-      </TodoProvider>
+          <TodoAddModal />
+        </TodoProvider>
+      </ToastProvider>
     </div>
   );
 }
--- src/pages/Kevin/TodoList/components/TodoContext.jsx
+++ src/pages/Kevin/TodoList/components/TodoContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useState, useContext } from 'react';
 import LocalStorageTodo from './../js/todo/localstorageTodo.js';
+import { useToast } from './Toast.jsx';
 
 export const TodoContext = createContext();
 
 export function TodoProvider({ children }) {
   const todo = new LocalStorageTodo('kevin-local-storage-todo-list', true);
+
+  const { showToast } = useToast();
 
   const [todoItems, setTodoItems] = useState(todo.todos);
   const [todoFilter, setTodoFilter] = useState('all');
@@ -36,16 +39,28 @@ export function TodoProvider({ children }) {
   const addTodo = (newItem) => {
     const { todos } = todo.add(newItem);
     setTodoItems(todos);
+    showToast({
+      message: 'New Task added!',
+      status: 0,
+    });
   };
 
   const editTodo = (itemId, { task, isCompleted }) => {
     const { todos } = todo.edit(itemId, { task, isCompleted });
     setTodoItems(todos);
+    showToast({
+      message: 'Task edited success!',
+      status: 0,
+    });
   };
 
   const removeTodo = (todoId) => {
     const { todos } = todo.remove(todoId);
     setTodoItems(todos);
+    showToast({
+      message: 'Task removed success!',
+      status: 0,
+    });
   };
 
   const value = { filteredTodoItems, listStatus, setTodoFilter, addTodo, editTodo, removeTodo };
