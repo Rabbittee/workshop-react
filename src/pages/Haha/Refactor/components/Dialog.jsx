511f18794fa540447fc05baedff5eb6a83638447
haha861924 <qq861924@gmail.com>
2022-04-10 01:41:48 +0800

    refactor: 💡 dialog title

--- src/pages/Haha/Refactor/components/Dialog.jsx
+++ src/pages/Haha/Refactor/components/Dialog.jsx
@@ -1,6 +1,5 @@
 import { useDialog } from '../store/DialogContext';
 import { useTodoList } from '../store/TodoListContext';
-import { Title } from '../utils/Constant';
 import Button from '../components/Button';
 
 export default function Dialog() {
@@ -17,7 +16,7 @@ export default function Dialog() {
           <div className="absolute inset-0 z-0 bg-black opacity-80"></div>
           <div className="relative  mx-auto my-auto w-full max-w-lg rounded-xl bg-white p-5  shadow-lg ">
             <div className="flex-auto justify-center p-5 text-center">
-              <h2 className="py-4 text-xl font-bold ">{Title}</h2>
+              <h2 className="py-4 text-xl font-bold ">Do you sure delete?</h2>
             </div>
             <div className="space-x-4 text-center md:block">
               <Button text="cancel" onClick={cancel} />
--- src/pages/Haha/Refactor/utils/Constant.jsx
+++
@@ -1,1 +0,0 @@
-export const Title = 'Do you sure delete?';
